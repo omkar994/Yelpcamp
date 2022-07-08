32ddb1ded7efa8054aa58c7457b21c135a1274d4
omkar <rautomkar4@gmail.com>
2022-07-08 22:43:27 +0530

    checkpoint 2

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express=require('express');
 const app=express();
 const path=require('path');
 const methodOverride=require('method-override');
+const engine = require('ejs-mate');
 const mongoose=require('mongoose');
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
     useNewUrlParser:true,
@@ -27,7 +28,7 @@ app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
-
+app.engine('ejs', engine);
 app.get('/',(req,res)=>{
     res.render('home');
 });
--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price=Math.floor(Math.random() * 100);
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image:'https://cdn.pixabay.com/photo/2020/03/15/03/07/camping-4932314_960_720.jpg',
+            price:price,
+            description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.',
         })
         await camp.save();
     }
